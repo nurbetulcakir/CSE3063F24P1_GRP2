3394b62d5aa8f7031aa5b0fd97909362620661a1
Nurbetül Çakır <106403877+nurbetulcakir@users.noreply.github.com>
2024-11-07 00:02:18 +0300

    Add files via upload
    Created Sample Data and RegistrationData classes
    Modified Main class

---
+++ src/RegistrationData.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+class RegistrationData {
+	List<Student> students;
+	List<Course> courses;
+	
+	public RegistrationData(List<Student> students) {
+		this.students = students;
+//		this.courses = courses;
+	}
+}
