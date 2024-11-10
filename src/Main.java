21b1a67d18ded7331a744c61175000096396338d
Turgut Köroğlu <116144652+Turgutkoroglu@users.noreply.github.com>
2024-11-11 00:05:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,8 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) {
 		
-		ObjectCreator oc = new ObjectCreator();
-		oc.createObjects();
-		
 		UserInterface ui = new UserInterface();
 		ui.login();
 
-		for (int i = 0; i < oc.students.size(); i++) {
-			System.out.println(oc.students.get(i));
-		}
 	}
 }
