0bfb8ebe12bf50b3c73ba91f5debe62fa1881b22
Nurbetul Cakir <nurbetul.cakir@gmail.com>
2024-11-30 23:16:45 +0300

    Working on CourseSession and CourseSection

--- src2/CourseSection.java
+++ src2/CourseSection.java
@@ -4,19 +4,27 @@ public class CourseSection{
     private int sectionID;
     private Course course; // Composition relationship with Course
     private ArrayList<Student> enrolledStudents = new ArrayList<Student>();
+    private ArrayList<CourseSession> courseSessions = new ArrayList<CourseSession>();
     private int capacity;
     // Constructor
     
-    public CourseSection(int sectionID, Course course, ArrayList<Student> enrolledStudents, int capacity) {
+    public CourseSection(int sectionID, Course course, ArrayList<Student> enrolledStudents) {
         this.setSectionID(sectionID);
         this.setCourse(course); // Course is a required component for CourseSection (Composition)
         this.setCapacity(capacity);
         this.setEnrolledStudents(enrolledStudents); // Start as an empty list
     }
     
-    public CourseSection(int sectionID, Course course, int capacity) {
+    public CourseSection(int sectionID, Course course, int capacity, ArrayList<CourseSession> courseSessions) {
         this.setSectionID(sectionID);
         this.setCourse(course); // Course is a required component for CourseSection (Composition)
+        this.setCapacity(capacity);
+        this.setCourseSessions(courseSessions);
+    }
+    
+    public CourseSection(int sectionID, Course course, int capacity)  {
+        this.setSectionID(sectionID);
+        this.setCourse(course);
         this.setCapacity(capacity);
     }
 
@@ -50,4 +58,11 @@ public class CourseSection{
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
+	public ArrayList<CourseSession> getCourseSessions() {
+		return this.courseSessions;
+	}
+	public void setCourseSessions(ArrayList<CourseSession> courseSessions) {
+		this.courseSessions = courseSessions;
+	}
+	
  }
